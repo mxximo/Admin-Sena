3aa85a59e4896dbb4875a14128e741c4261fbda9
Camilo <baironramos959@gmail.com>
2025-07-04 17:33:03 -0500

    ultimo retoque

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route; // ✅ ESTA LÍNEA FALTABA
+use Illuminate\Support\Facades\Route; 
 
 class RouteServiceProvider extends ServiceProvider
 {
